7017f34adbfa4faca30af84c192ccdc4e03185f1
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2022-11-05 15:12:42 +0100

    Update question-2.js
    getting error.. dont know what I´m doing wrong yet

--- js/question-2.js
+++ js/question-2.js
@@ -1,32 +1,35 @@
-const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=3b413b2f3a9f43b6aa2baee45dafdfcb`;
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=3b413b2f3a9f43b6aa2baee45dafdfcb";
 
 
 const dataContainer = document.querySelector(".data");
 
-async function getData(url) {
+async function getData() {
     try {
         const response = await fetch(url);
         
         const data = await response.json();
 
         console.log(data);
+
+        const games = games.results;
    
-        dataContainer.innerHTML= "";
+        dataContainer.innerHTML = "";
+
+        for (let i = 0; i < games.length; i++) {
+            console.log(games[i].name);
+
+            if (i === 7) {
+              break;
+        }
+
+        dataContainer.innerHTML += `<div class="data">${games[i].name}</div>`;
+    }
 
     } catch (error) {
         console.log(error);
         dataContainer.innerHTML =alert("error", error);
     }
 
-    //for (let i = 0; i < games.length; i++) {
-       // console.log(games[i].name);
-
-        //if (i === 7) {
-           // break;
-        //}
-
-       // dataContainer.innerHTML += `<div class="data">${games[i].text}</div>`;
-    //}
 }
 
 getData();
